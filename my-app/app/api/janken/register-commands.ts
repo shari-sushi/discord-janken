6b116f5ac18ef12cd963e547c66298d1bc35330e
shari-sushi <susi.imo.niku@gmail.com>
2026-01-28 03:37:17 +0900

    お試し

---
+++ my-app/app/api/janken/register-commands.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN!;
+const APPLICATION_ID = process.env.DISCORD_APPLICATION_ID!;
+
+const command = {
+  name: "submit",
+  description: "同時発言に参加",
+  options: [
+    {
+      name: "message",
+      description: "発言内容",
+      type: 3,
+      required: true,
+    },
+  ],
+};
+
+fetch(`https://discord.com/api/v10/applications/${APPLICATION_ID}/commands`, {
+  method: "POST",
+  headers: {
+    Authorization: `Bot ${DISCORD_TOKEN}`,
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(command),
+})
+  .then((res) => res.json())
+  .then(console.log);
